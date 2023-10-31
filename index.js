f70a1b76acace54db74cf8374040adf7681ae8e2
Анна Артемьева <artemiden@Annas-MacBook-Pro.local>
2023-10-31 07:28:59 -0400

    added styling to the module

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ import {
     html
 } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js'
 
+import {
+    css
+} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js'
+
 // defining a custom element for the Header
 class MyHeader extends LitElement {
+
+    static styles = css `
+    #toggle-button {
+    width: 200px;
+    height: 50px;
+    border-radius: 10px;
+    color: white;
+    background: rgb(29, 38, 113);
+    margin: 10px;
+    font-size: 16px;
+    }
+  `
+
     constructor() {
         super()
         this.isModalOpen = false
@@ -57,28 +74,64 @@ Header.addEventListener('toggle-form', () => {
 
 // defining a custom element for the form
 class MyForm extends LitElement {
+    static styles = css `
+    .my-form {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 20px;
+        font-size: 18px;
+    }
 
-    // render the form    
+    .my-form label {
+        font-size: 24px;
+        margin-bottom: 10px;
+        color: white;
+    }
+
+    .my-form input[type="text"],
+    .my-form select {
+        width: 100%;
+        max-width: 250px;
+        font-size: 18px;
+        padding: 14px;
+        margin: 10px 0;
+    }
+
+    .my-form input[type="checkbox"],
+    .my-form input[type="radio"] {
+        width: 18px;
+        height: 18px;
+    }
+`
+
     render() {
         return html `
-      <form>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name"><br><br>
-        <label for="country">Country:</label>
-        <select id="country" name="country">
-          <option value="usa">USA</option>
-          <option value="canada">Canada</option>
-          <option value="uk">UK</option>
-        </select><br><br>
-        <label for="subscribe">Subscribe to newsletter:</label>
-        <input type="checkbox" id="subscribe" name="subscribe" value="yes"><br><br>
-        <label>Gender:</label><br>
-        <input type="radio" id="male" name="gender" value="male">
-        <label for="male">Male</label><br>
-        <input type="radio" id="female" name="gender" value="female">
-        <label for="female">Female</label><br>
-      </form>
-    `
+            <form class="my-form">
+                <div>
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" autocomplete="off">
+                </div>
+                <div>
+                    <label for="country">Country</label>
+                    <select id="country" name="country">
+                        <option value="usa">USA</option>
+                        <option value="canada">Canada</option>
+                        <option value="uk">UK</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="subscribe">Subscribe to newsletter</label>
+                    <input type="checkbox" id="subscribe" name="subscribe" value="yes">
+                </div>
+                <div>
+                    <label>Gender</label><br>
+                    <input type="radio" id="male" name="gender" value="male">
+                    <label for="male">Male</label><br>
+                    <input type="radio" id="female" name="gender" value="female">
+                    <label for="female">Female</label>
+                </div>
+            </form>
+        `
     }
 }
 
@@ -89,12 +142,32 @@ customElements.define('my-form', MyForm)
 // define a customer element for the footer
 class MyFooter extends LitElement {
 
-    //render the footer    
+    static styles = css `
+    .my-button {
+    width: 200px;
+    height: 50px;
+    border-radius: 10px;
+    color: white;
+    background: rgb(29, 38, 113);
+    margin: 10px;
+    font-size: 16px;
+    box-shadow: 1px 2px 15px white;
+    }
+
+    #footer {
+        padding-left: 20px;
+    }
+
+  `
+
+
+
+     // render the footer    
     render() {
         return html `
       <div id="footer">
-        <button id="cancelButton">Cancel</button>
-        <button id="submitButton">Submit</button>
+        <button id="cancelButton" class="my-button">Cancel</button>
+        <button id="submitButton" class="my-button">Submit</button>
       </div>
     `
     }
